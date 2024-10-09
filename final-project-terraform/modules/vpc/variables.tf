48826cf69f05124a8aba296d83537490b9392bd6
rtzi-hub <ron.tzidky@nitzanim.tech>
2024-10-09 21:46:00 +0300

    Delete final-project-terraform directory

--- final-project-terraform/modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
-
-output "eks_node_sg_id" {
-  description = "The security group ID for the EKS worker nodes."
-  value       = aws_security_group.eks_node_sg.id
-}
