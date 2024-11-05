34fea4f21f8e2abfa96bce01d5c4a36a26256796
rtz-hub <ron.tzidky@nitzanim.tech>
2024-11-05 14:55:14 +0200

    "Changing the subnets for the EKS to Private subnets only"

--- final-project-terraform/main.tf
+++ final-project-terraform/main.tf
@@ -29,16 +29,17 @@ module "rds" {
 
 # EKS Module
 module "eks" {
-  source                     = "./modules/eks"
-  vpc_id                     = module.vpc.vpc_id
-  subnet_ids = concat(module.vpc.public_subnet_ids, module.vpc.private_subnet_ids)
-  security_group_ids         = [module.vpc.eks_node_sg_id]
-  eks_cluster_role_arn       = module.iam.eks_cluster_role_arn
-  eks_node_group_role_arn    = module.iam.eks_node_group_role_arn
+  source                         = "./modules/eks"
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnet_ids
+  security_group_ids             = [module.vpc.eks_node_sg_id]
+  eks_cluster_role_arn           = module.iam.eks_cluster_role_arn
+  eks_node_group_role_arn        = module.iam.eks_node_group_role_arn
   eks_worker_node_policy_attachment = module.iam.eks_worker_node_policy_attachment
   eks_cni_policy_attachment         = module.iam.eks_cni_policy_attachment
   ecr_read_only_policy_attachment   = module.iam.ecr_read_only_policy_attachment
 }
+
 
 # # Load Balancer Module
 # module "load_balancer" {
--- final-project-terraform/modules/eks/main.tf
+++ final-project-terraform/modules/eks/main.tf
@@ -17,7 +17,7 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = var.node_group_name
   node_role_arn   = var.eks_node_group_role_arn
-  subnet_ids      = var.subnet_ids
+  subnet_ids      = var.private_subnet_ids
 
   scaling_config {
     desired_size = var.desired_capacity
